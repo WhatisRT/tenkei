ba87f9109b3601fe6fc4804e0b9f480af9d8b120
Andre Knispel <andre.knispel@gmx.de>
2018-03-08 17:11:57 +0100

    First working version

--- test_call.hs
+++ test_call.hs
@@ -8,19 +8,39 @@ import Foreign.Ptr
 import Data.CBOR
 import Data.Binary.CBOR
 
-#include "tenkei_rust.h"
+import Data.ByteString.Lazy (ByteString, unpack, pack)
+
+import Data.Binary.Put
+import Data.Binary.Get
+
+import Data.Maybe
+
+-- #include "rust/src/tenkei_rust.h"
 
 foreign import ccall "triple" c_triple :: Ptr Word8 -> CSize -> Ptr (Ptr Word8) -> Ptr CSize -> IO ()
 	
-serializeInt :: Int -> [Word8]
-serializeInt = unpack . runPut . putCBOR . CBOR_UInt
+serializeInt :: Integer -> [Word8]
+serializeInt = unpack . runPut . putCBOR . intToCBOR
 
-testCall :: [Word8] -> IO [Word8]
-testCall bytes = withArray bytes $ \ptr ->
-               		do
-						let res_ptr = Ptr nullPtr
-							res_size = Ptr 0
-						in
-							c_triple ptr (fromIntegral (length bytes)) res_ptr res_size
-							res_size' <- peek res_size
-                 	  		peekArray (fromEnum res_size') res_ptr
+intToCBOR :: Integer -> CBOR
+intToCBOR i | i >= 0 = CBOR_UInt i
+			| otherwise = CBOR_SInt i
+
+deserializeInt :: [Word8] -> Integer
+deserializeInt i = fromJust $ cborToInt $ runGet getCBOR $ pack i
+
+cborToInt :: CBOR -> Maybe Integer
+cborToInt (CBOR_UInt i) = Just i
+cborToInt (CBOR_SInt i) = Just i
+cborToInt _ = Nothing
+
+f = (fmap deserializeInt) . (call c_triple) . serializeInt
+
+call :: (Ptr Word8 -> CSize -> Ptr (Ptr Word8) -> Ptr CSize -> IO ()) -> [Word8] -> IO [Word8]
+call function bytes = withArray bytes $ \ptr -> (alloca (\res_ptr -> alloca (\res_size ->
+					do
+						function ptr (fromIntegral (length bytes)) res_ptr res_size
+						res_ptr' <- peek res_ptr
+						res_size' <- peek res_size
+						peekArray (fromEnum res_size') res_ptr'
+					)))
