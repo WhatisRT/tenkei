4ef641dd1fff88044d94d3122f6aa5bdbd1b9d5b
Tobias Bucher <tobiasbucher5991@gmail.com>
2018-03-08 15:55:05 +0100

    Add Rust implementation

---
+++ rust/src/lib.rs
@@ -0,0 +1,31 @@
+extern crate serde_cbor as cbor;
+
+use std::mem;
+use std::slice;
+
+fn triple(x: i32) -> i32 {
+    x * 3
+}
+
+fn triple_tenkei_impl(input: &[u8]) -> Box<[u8]> {
+    let param = cbor::from_slice(input).unwrap();
+    cbor::to_vec(&triple(param)).unwrap().into_boxed_slice()
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn triple_tenkei(
+    input: *const u8,
+    input_len: usize,
+    output: *mut *mut u8,
+    output_len: *mut usize,
+) {
+    let mut result = triple_tenkei_impl(slice::from_raw_parts(input, input_len));
+    *output = result.as_mut_ptr();
+    *output_len = result.len();
+    mem::forget(result)
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn tenkei_free(buffer: *mut u8, buffer_len: usize) {
+    mem::drop(Box::from_raw(slice::from_raw_parts_mut(buffer, buffer_len) as *mut [u8]));
+}
