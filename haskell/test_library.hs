ba8604d53ce9bf5630203cb680b1fcda8bbc7d7d
Andre Knispel <andre.knispel@gmx.de>
2018-03-09 13:39:14 +0100

    Add a library that is to be called from C.

---
+++ haskell/test_library.hs
@@ -0,0 +1,52 @@
+module Test where
+
+import Foreign
+import Foreign.C
+import Foreign.Ptr
+import Foreign.Marshal.Alloc
+
+import Data.CBOR
+import Data.Binary.CBOR
+
+import Data.ByteString.Lazy (ByteString, unpack, pack)
+
+import Data.Binary.Put
+import Data.Binary.Get
+
+import Data.Maybe
+
+foreign export ccall tenkei_free :: Ptr Word8 -> CSize -> IO ()
+foreign export ccall quadruple :: Ptr Word8 -> CSize -> Ptr (Ptr Word8) -> Ptr CSize -> IO ()
+
+tenkei_free :: Ptr Word8 -> CSize -> IO ()
+tenkei_free args size = free args
+
+quadruple :: Ptr Word8 -> CSize -> Ptr (Ptr Word8) -> Ptr CSize -> IO ()
+quadruple args argn res resn = do 
+                                x <- fmap (serializeInt . fromIntegral . quadrupleImpl . fromJust . cborToInt) $ fromC args argn
+                                poke resn $ fromIntegral $ length x
+                                res_ptr <- newArray x
+                                poke res $ res_ptr
+                                   
+                                    
+                                    
+
+quadrupleImpl x = 4 * x
+                                    
+fromC :: Ptr Word8 -> CSize -> IO CBOR
+fromC args argn = fmap toCBOR $ peekArray (fromEnum argn) args
+
+toCBOR :: [Word8] -> CBOR
+toCBOR = runGet getCBOR . pack
+
+cborToInt :: CBOR -> Maybe Integer
+cborToInt (CBOR_UInt i) = Just i
+cborToInt (CBOR_SInt i) = Just i
+cborToInt _ = Nothing
+
+serializeInt :: Int32 -> [Word8]
+serializeInt = unpack . runPut . putCBOR . intToCBOR
+
+intToCBOR :: Int32 -> CBOR
+intToCBOR i | i >= 0 = CBOR_UInt $ fromIntegral i
+            | otherwise = CBOR_SInt $ fromIntegral i
