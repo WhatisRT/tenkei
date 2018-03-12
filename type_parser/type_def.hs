80c0d70212e969b2b340dfe402559596bed8ed00
Tobias Bucher <tobiasbucher5991@gmail.com>
2018-03-12 14:47:16 +0100

    type_parser: Add a couple of convenience functions

--- type_parser/type_def.hs
+++ type_parser/type_def.hs
@@ -1,3 +1,33 @@
+{-# OPTIONS_GHC -Wall #-}
+
 module TypeDef where
 
-type TypeDef = TypeDefSum { name :: String, parts :: [(String,String)] } | TypeDefProduct { name :: String, parts :: [String] }
+import Data.Char
+import Data.Foldable
+import Data.List
+
+type Identifier = [String]
+data TypeDef =
+      TypeDefSum { name :: String, sumParts :: [(String,String)] }
+    | TypeDefProduct { name :: String, prodParts :: [String] }
+    deriving Show
+
+title :: String -> String
+title (c:cs) = toUpper c : cs
+title [] = []
+
+upper :: String -> String
+upper = fmap toUpper
+
+camelCase :: Identifier -> String
+camelCase (n:ns) = n ++ pascalCase ns
+camelCase [] = []
+
+snakeCase :: Identifier -> String
+snakeCase = fold . intersperse "_"
+
+screamingSnakeCase :: Identifier -> String
+screamingSnakeCase = fold . intersperse "_" . fmap upper
+
+pascalCase :: Identifier -> String
+pascalCase = fold . fmap title
