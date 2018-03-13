4712e46cbdb53d42810a3fc11e9b286403ac4f6a
Andre Knispel <andre.knispel@gmx.de>
2018-03-13 23:19:33 +0100

    Add encoding and decoding type definitions with JSON.

--- type_parser/TypeDef.hs
+++ type_parser/TypeDef.hs
@@ -1,4 +1,6 @@
 {-# OPTIONS_GHC -Wall #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE FlexibleInstances #-}
 
 module TypeDef where
 
@@ -6,11 +8,36 @@ import Data.Char
 import Data.Foldable
 import Data.List
 
+import Data.Aeson
+import Data.ByteString.Lazy.Char8 (pack)
+
 type Identifier = [String]
-data TypeDef =
-      TypeDefSum { name :: String, sumParts :: [(String,String)] }
-    | TypeDefProduct { name :: String, prodParts :: [String] }
-    deriving Show
+data TypeDef = TypeDef { name :: String, parts :: TypeParts } deriving Show
+data TypeParts = SumParts [(String,String)] | ProdParts [String] | Unit deriving Show
+
+decodeType :: String -> Maybe TypeDef
+decodeType = decode . pack
+
+toTypeDef :: String -> Maybe [(String,String)] -> Maybe [String] -> TypeDef
+toTypeDef n (Just x) _ = TypeDef n $ SumParts x
+toTypeDef n _ (Just x) = TypeDef n $ ProdParts x
+toTypeDef n _ _ = TypeDef n Unit
+
+instance ToJSON TypeDef where
+  toJSON x = object ["name" .= name x, partsType .= parts x]
+    where partsType = case (parts x) of
+            SumParts _ -> "sumParts"
+            ProdParts _ -> "prodParts"
+            Unit -> "prodParts"
+
+instance ToJSON TypeParts where
+  toJSON (SumParts x) = toJSON x
+  toJSON (ProdParts x) = toJSON x
+  toJSON Unit = toJSON ([] :: [String])
+
+instance FromJSON TypeDef where
+  parseJSON = withObject "TypeDef" $ \v ->
+    toTypeDef <$> v .: "name" <*> v .:? "sumParts" <*> v .:? "prodParts"
 
 title :: String -> String
 title (c:cs) = toUpper c : cs
