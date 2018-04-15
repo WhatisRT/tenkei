048f6ca8d1e34488cb1926d1158bd8f08ba91f28
Andre Knispel <andre.knispel@gmx.de>
2018-04-15 16:26:50 +0200

    Modify the python output to be compatible with the other executables

--- python/app/main_test.py
+++ python/app/main_test.py
@@ -2,11 +2,20 @@ import test_library
 
 test_list = [1,2,3,4,5,6,7,8,9,10]
 
+def print_bool(b):
+    if b:
+        print("True")
+    else:
+        print("False")
+
+def print_list(l):
+    print("[" + (''.join(str(c) + "," for c in l)[:-1] + "]"))
+
 print("python")
 print(''.join(str(chr(e)) for e in test_library.library_language()))
-print(test_library.binary_or(1, 0))
-print(test_library.modify_array(test_list))
-print(test_library.exponentiate(2,3))
-print(test_library.identity(test_list))
+print_bool(test_library.binary_or(1, 0))
+print_list(test_library.modify_array(test_list))
+print_list(test_library.exponentiate(2,x) for x in test_list)
+print_list(test_library.identity(test_list))
 print(test_library.choose_left(1,2))
-print(test_library.reverse_list(test_list))
+print_list(test_library.reverse_list(test_list))
