9d4a56084cd6fd27ebde22b6745172b89b48cad3
Andre Knispel <andre.knispel@gmx.de>
2018-04-15 12:15:43 +0200

    Make TenkeiPtr a newtype

--- haskell/libtenkei-haskell/src/Pointers.hs
+++ haskell/libtenkei-haskell/src/Pointers.hs
@@ -1,15 +1,15 @@
 {-# LANGUAGE TypeSynonymInstances #-}
 {-# LANGUAGE FlexibleInstances #-}
 
-module Pointers (TenkeiPtr) where
+module Pointers where
 
 import Foreign
 import Data.CBOR
 import Tenkei
 
-type TenkeiPtr = Ptr ()
+newtype TenkeiPtr = TenkeiPtr { getPtr :: Ptr ()}
 
 instance Tenkei TenkeiPtr where
-  serialize = CBOR_UInt . fromIntegral . (\(WordPtr x) -> x) . ptrToWordPtr
-  deserialize (CBOR_UInt i) = wordPtrToPtr $ WordPtr $ fromIntegral i
+  serialize = CBOR_UInt . fromIntegral . (\(WordPtr x) -> x) . ptrToWordPtr . getPtr
+  deserialize (CBOR_UInt i) = TenkeiPtr $ wordPtrToPtr $ WordPtr $ fromIntegral i
   deserialize x = error ("Error while interpreting CBOR: not a memory address:\n" ++ show x)
