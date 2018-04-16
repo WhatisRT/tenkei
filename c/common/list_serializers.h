c7da158cd83ec7a148487af927d56ba7322b679e
Andre Knispel <andre.knispel@gmx.de>
2018-04-16 15:37:11 +0200

    Add missing headers

---
+++ c/common/list_serializers.h
@@ -0,0 +1,6 @@
+#ifndef __list_serializers__
+#define __list_serializers__
+
+#include "list_serializers.c"
+
+#endif
---
+++ c/common/list_types.h
@@ -0,0 +1,19 @@
+#ifndef __list_types__
+#define __list_types__
+
+struct list_int32_t {
+  int32_t *start;
+  unsigned int length;
+};
+
+struct list_tenkei_ptr {
+  void * *start;
+  unsigned int length;
+};
+
+struct list_tenkei_value {
+  struct tenkei_value *start;
+  unsigned int length;
+};
+
+#endif
