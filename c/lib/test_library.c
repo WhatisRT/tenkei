5d679fb455c385db973316b56e559687f5a029be
Andre Knispel <andre.knispel@gmx.de>
2018-04-18 16:06:36 +0200

    Declare functions as static, to fix problems on linux

--- c/lib/test_library.c
+++ c/lib/test_library.c
@@ -5,7 +5,7 @@
 //#include "../libtenkei-c/ffi_wrappers.h"
 #include "../common/list_types.h"
 
-struct list_int32_t library_language()
+static struct list_int32_t library_language(void)
 {
   int32_t *list_ = malloc(sizeof(int32_t) * 1);
   struct list_int32_t res = {list_, 1};
@@ -14,12 +14,12 @@ struct list_int32_t library_language()
   return res;
 }
 
-bool binary_or(bool a, bool b)
+static bool binary_or(bool a, bool b)
 {
   return a || b;
 }
 
-struct list_int32_t modify_array(struct list_int32_t l)
+static struct list_int32_t modify_array(struct list_int32_t l)
 {
   for(int i = 0; i < l.length; i++)
   {
@@ -29,7 +29,7 @@ struct list_int32_t modify_array(struct list_int32_t l)
   return l;
 }
 
-int32_t exponentiate(int32_t x, int32_t y)
+static int32_t exponentiate(int32_t x, int32_t y)
 {
   int32_t acc = 1;
   for(int i = 0; i < y; i++)
@@ -38,17 +38,17 @@ int32_t exponentiate(int32_t x, int32_t y)
   return acc;
 }
 
-struct tenkei_value identity(struct tenkei_value x)
+static struct tenkei_value identity(struct tenkei_value x)
 {
   return x;
 }
 
-struct tenkei_value choose_left(struct tenkei_value l, struct tenkei_value r)
+static struct tenkei_value choose_left(struct tenkei_value l, struct tenkei_value r)
 {
   return l;
 }
 
-struct list_tenkei_value reverse_list(struct list_tenkei_value l)
+static struct list_tenkei_value reverse_list(struct list_tenkei_value l)
 {
   struct tenkei_value *list_ = malloc(sizeof(struct tenkei_value) * l.length);
   struct list_tenkei_value res = {list_, l.length};
@@ -59,7 +59,7 @@ struct list_tenkei_value reverse_list(struct list_tenkei_value l)
   return res;
 }
 
-struct tenkei_value apply_function(void (*f)(uint8_t *, size_t, uint8_t **, size_t *), struct tenkei_value x)
+static struct tenkei_value apply_function(void (*f)(uint8_t *, size_t, uint8_t **, size_t *), struct tenkei_value x)
 {
   cbor_item_t *arg = cbor_new_definite_array(1);
   cbor_array_push(arg, x.contents);
