d59d84a2ce00d81933676b300ee474838a94f22b
Andre Knispel <andre.knispel@gmx.de>
2018-04-20 20:05:19 +0200

    Remove unnecessary code

--- type-parser/src/Generators/Python.hs
+++ type-parser/src/Generators/Python.hs
@@ -72,8 +72,6 @@ generatePythonInterface' (DefFile _ funDefs _) =
   , "    \"\"\","
   , "                      libraries=['test-library'],"
   , "                      library_dirs=['../../tenkei-build'])"
-  , "                      #library_dirs=['./', '/Library/Frameworks/GHC.framework/Versions/Current/usr/lib/ghc-8.2.2/rts/'],"
-  , "                      #extra_compile_args=['-arch x86_64', '-Wl,-rpath=/Library/Frameworks/GHC.framework/Versions/Current/usr/lib/ghc-8.2.2/rts/'])"
   , ""
   , "ffibuilder.cdef(\"\"\""
   ] ++
